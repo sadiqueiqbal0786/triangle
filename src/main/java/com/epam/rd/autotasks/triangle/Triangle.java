a7beb039741d4639f5173489e03c7da3c8948352
Sadique Iqbal <sadiqueiqbal.si@gmail.com>
2023-01-27 12:40:20 +0530

    Done

--- src/main/java/com/epam/rd/autotasks/triangle/Triangle.java
+++ src/main/java/com/epam/rd/autotasks/triangle/Triangle.java
@@ -1,17 +1,47 @@
 package com.epam.rd.autotasks.triangle;
 
+import static java.awt.geom.Point2D.distance;
+import static java.lang.Math.sqrt;
+
 class Triangle {
+    Point a,b,c;
+
 
     public Triangle(Point a, Point b, Point c) {
-        //TODO
+       double p1 = length(a,b);
+       double p2 = length(b,c);
+       double p3 = length(a,c);
+       if((p1+p2)<=p3 || (p2+p3)<=p1 || (p3+p1)<=p2){
+           throw new IllegalArgumentException();
+       }
+           this.a=a;
+           this.b=b;
+           this.c=c;
+
     }
 
     public double area() {
-        //TODO
+
+        double p1 = length(a,b);
+        double p2 = length(b,c);
+        double p3 = length(a,c);
+        double s = (p1+p2+p3) / 2;
+        return Math.sqrt(s * (s - p1) * (s - p2) * (s - p3));
     }
 
     public Point centroid(){
-        //TODO
+
+        double x = (a.getX() + b.getX() + c.getX()) / 3;
+        double y = (a.getY() + b.getY() + c.getY()) / 3;
+
+        return new Point(x, y);
+    }
+    public double length(Point a, Point b)
+    {
+        // double distance = Math.sqrt(Math.pow((b.getX()-a.getX()),2)+Math.pow((b.getY()-a.getY()),2));
+        double xDiff = b.getX() - a.getX();
+        double yDiff = b.getY() - a.getY();
+        return Math.sqrt(xDiff * xDiff + yDiff * yDiff);
     }
 
 }
